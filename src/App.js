50da742bda9dd5ba3097d0293fb77fe2aeb6e9ef
Christopher Seal <christopher.seal@shopify.com>
2021-02-25 14:16:00 -0500

    adding theme toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Home} from './sections/Home';
 import {
@@ -8,12 +9,44 @@ import {
   Link
 } from "react-router-dom";
 
+const ThemeContext = React.createContext('light');
 function App() {
-  return ( <Router> <Switch>
-    <Route exact path="/">
-      <Home title="Hello World"/>
-    </Route>
-  </Switch> </Router> );
+
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    let root = document.documentElement;
+    if (theme=="dark") {
+      root.style.setProperty('--background',  '#000');
+      root.style.setProperty('--text-color', '#fff');
+    }
+    else {
+      root.style.setProperty('--background',  '#fff');
+      root.style.setProperty('--text-color', '#000');
+    }
+  }, [theme]);
+
+  const toggleTheme=()=>{
+    setTheme((currentTheme)=>{
+      return currentTheme=="dark"?
+      "light":
+      "dark"
+    })
+  }
+
+  return ( <ThemeContext.Provider value={theme}>
+    <button onClick={toggleTheme}>
+      Theme
+    </button>
+    <Router> 
+      <Switch>
+        <Route exact path="/">
+        <Home title="Hello World"/>
+      </Route>
+    </Switch> 
+    </Router>
+  </ThemeContext.Provider>
+     );
 }
 
 export default App;
