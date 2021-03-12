961d5dadafa32d355cc8ea2632c1100aea71eae2
Christopher Seal <christopher.seal@shopify.com>
2021-03-12 12:13:55 -0500

    fix image size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,11 @@ import './hero.css';
 export function Hero({ title, text, imgUrl }) {
   return (
     <article className='hero'>
+      <img src={imgUrl} />
       <div>
         <h2>{title}</h2>
         <p>{text}</p>
       </div>
-      <img src={imgUrl} />
     </article>
   );
 }
