6bad5d09845f920bc09a7896e7d8e6876165ceef
Christopher Seal <christopher.seal@shopify.com>
2021-04-08 14:53:28 -0400

    more links

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -3,20 +3,20 @@ import { Hero } from "../components/Hero";
 import { LayoutSection } from "../components/LayoutSection";
 import { SocialIcons } from "../components/SocialIcons"
 
-export function Home({ title }) {
+export function Home() {
   const img = require("../images/main_image.jpg");
   return (
     <>
     <LayoutSection>
-      <h1> {title} </h1>
+      {/* <h1> {title} </h1> */}
       <Hero
         imgUrl={img.default}
         imgAlignment="hero--imgAlignLeft"
       >
         <div>
-        <h2>Welcome</h2>
+        <h2>Christopher Seal</h2>
         <SocialIcons></SocialIcons>
-        <p>"I have an approximate knowledge of many things. I'm currently a developer at Shopify and I'm still building this site.."</p>
+        <p>I have an approximate knowledge of many things. I'm currently a software developer at Shopify.</p>
         </div>
       </Hero>
       </LayoutSection>
