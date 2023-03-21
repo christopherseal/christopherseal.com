3c87bcbf0e194159c780c8cfa203b8466e85717f
Christopher Seal <christopher.seal@shopify.com>
2023-03-21 14:29:01 -0400

    fix toggle button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import {Home} from './sections/Home';
@@ -6,47 +5,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 const ThemeContext = React.createContext('light');
 function App() {
 
   const [theme, setTheme] = useState("light");
+let currentTheme = theme;
 
-  useEffect(() => {
-    let root = document.documentElement;
-    if (theme=="dark") {
-      root.style.setProperty('--background',  '#000');
-      root.style.setProperty('--text-color', '#fff');
-    }
-    else {
-      root.style.setProperty('--background',  '#fff');
-      root.style.setProperty('--text-color', '#000');
-    }
-  }, [theme]);
+useEffect(() => {
+  let root = document.documentElement;
+  root.style.setProperty('--text-color', theme === "dark" ? "#fff" : "#000");
+  root.style.setProperty('--background', theme === "dark" ? "#000" : "#fff");
+}, [theme]);
 
-  const toggleTheme=()=>{
-    setTheme((currentTheme)=>{
-      return currentTheme=="dark"?
-      "light":
-      "dark"
-    })
-  }
-
-  return ( <ThemeContext.Provider value={theme}>
-    {/* <button onClick={toggleTheme}>
-      Theme
-    </button> */}
-    <Router> 
-      <Switch>
-        <Route exact path="/">
-        <Home title="Christopher Seal "/>
-      </Route>
-    </Switch> 
-    </Router>
-  </ThemeContext.Provider>
-     );
+const toggleTheme=()=>{
+  setTheme(theme === "dark" ? "light" : "dark");
+  currentTheme = currentTheme === "dark"?
+  "light":
+  "dark"
 }
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div className="toggleButton">
+        <button onClick={toggleTheme}>
+          <span className="circle" />
+        </button>
+      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home title="Christopher Seal "/>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeContext.Provider>
+  );
+  }
 
 export default App;
